cafb6233f5112163e7d5e41dd2597e3914208b85
zhaleh <zhalehelmi@gmail.com>
2023-12-01 19:58:50 +0330

    submitHandler function complete

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-//-- counter componet --
+//-- form componet --
 
 const textareaEl = document.querySelector(".form__textarea");
 const counterEl = document.querySelector(".counter");
+const formEl = document.querySelector(".form");
 
 const inputHandler = () => {
   //determine maximum number of characters
@@ -18,3 +19,28 @@ const inputHandler = () => {
 };
 
 textareaEl.addEventListener("input", inputHandler);
+
+//-- counter componet --
+const submitHandler = (event) => {
+  event.preventDefault();
+
+  //get text from textarea
+  const text = textareaEl.value;
+
+  //validate text
+  if (text.includes("#") && text.length >= 5) {
+    formEl.classList.add("form--valid");
+
+    setTimeout(() => {
+      formEl.classList.remove("form--valid");
+    }, 2000);
+  } else {
+    formEl.classList.add("form--invalid");
+
+    setTimeout(() => {
+      formEl.classList.remove("form--invalid");
+    }, 2000);
+  }
+};
+
+formEl.addEventListener("submit", submitHandler);
